534aad099e43f9479a546ee4b4f25b035417c741
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-17 06:32:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+var leastFavoriteCustomer = "sue";
+
+function upperCaseCustomerName(customerName) {
+  window.customerName = window.customerName.toUpperCase();
+  return window.customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return window.bestCustomer;
+}
+
+function overwritBestCustomer() {
+  window.bestCustomer = 'maybe bob';
+  return window.bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  window.leastFavoriteCustomer = 'bill';
+  return window.leastFavoriteCustomer;
+}
